c9deb5af6551d3f71623d8cb465497d8f286819d
Katakung95 <tanwasripan@hotmail.co.uk>
2022-03-10 13:36:41 +0000

    implemented color scale with scaleQuantize, works and pass all tests

--- src/routes/Choroplethmap.js
+++ src/routes/Choroplethmap.js
@@ -38,13 +38,14 @@ export default function Choroplethmap() {
       const minPercent = window.d3.min(educationData, d => d.bachelorsOrHigher);
       const maxPercent = window.d3.max(educationData, d => d.bachelorsOrHigher);
 
-      const colorScheme = window.d3.scaleSequential()
+      const colors = ["#fce0a7", "#fad17d", "#f9c75f", "#f7b733", "#fa8629", "#fb6a23", "#fc4d1d"]
+
+      const colorScheme = window.d3.scaleQuantize()
         .domain([minPercent, maxPercent])
-        .interpolator(window.d3.interpolateOranges);
+        .range(colors);
 
       svg.attr("width", w)
         .attr("height", h)
-        .style("background-color", "white")
         .append("g")
         .attr("id", "map-counties")
         .selectAll("path")
@@ -85,42 +86,35 @@ export default function Choroplethmap() {
         // hide highlight of cell
         e.target.style.stroke = "none";
       }
-      // setting up linear gradient
-      const defs = svg.append("defs");
-      const linearGradient = defs.append("linearGradient")
-        .attr("id", "linear-gradient");
-      linearGradient
-        .attr("x1", "0%")
-        .attr("y1", "0%")
-        .attr("x2", "100%")
-        .attr("y2", "0%")
-        .append("stop")
-        .attr("offset", "0%")
-        .attr("stop-color", colorScheme.range()[0]);
-      linearGradient.append("stop")
-        .attr("offset", "100%")
-        .attr("stop-color", colorScheme.range()[1]);
+
       // setting up legend
+      const legendWidth = 250;
       const legendXscale = window.d3.scaleLinear()
         .domain([minPercent, maxPercent])
-        .range([0, 250]);
+        .range([0, legendWidth]);
       const legendAxis = window.d3.axisBottom()
         .scale(legendXscale)
-        .tickValues(window.d3.range(minPercent, maxPercent, (maxPercent - minPercent) / 8)
-          .map(boundary => Math.round(boundary)))
-        .tickFormat(d => d + "%")
+        .tickValues(colorScheme.thresholds())
+        .tickFormat(d => Math.round(d).toFixed(1) + "%")
+        .tickSize(-10)
+      const threshold = colorScheme.thresholds();
+      threshold.unshift(minPercent)
 
-      legend.call(legendAxis)
-        .attr("transform", "translate(600, 30)")
+      legend.attr("transform", "translate(600, 30)")
+        .append("g")
+        .selectAll("rect")
+        .data(threshold)
+        .enter()
         .append("rect")
+        .attr("width", legendWidth / threshold.length)
         .attr("height", 10)
-        .attr("width", 250)
-        .style("fill", "url(#linear-gradient)")
-        .attr("transform", "translate(0, -10)");
-      //legend.
+        .attr("x", (d, i) => i * (legendWidth / threshold.length))
+        .attr("y", -10)
+        .style("fill", d => colorScheme(d));
+      legend.call(legendAxis);
 
-      console.log(window.d3.range(minPercent, maxPercent, (maxPercent - minPercent) / 8).map(boundary => Math.round(boundary)))
-      console.log(colorScheme(50))
+      legend.select(".domain")
+        .remove();
     }
 
     if (educationData && geoJson) drawGraph();
