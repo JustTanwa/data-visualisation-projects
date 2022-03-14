ce472adcc7a1b88e00c5790dcc081dc2f1e352cb
Katakung95 <tanwasripan@hotmail.co.uk>
2022-03-14 12:59:12 +0000

    refractor treemap items into groups and changed legend layout

--- src/routes/Treemap.js
+++ src/routes/Treemap.js
@@ -10,10 +10,15 @@ export default function Treemap() {
   const [movieSaleData, setMovieSaleData] = useState(null);
   const [kickstarterData, setkickstarterData] = useState(null);
   const [categories, setCategories] = useState({})
+  const [curCategories, setCurCategories] = useState([])
 
   const gameSalesURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
   const movieSalesURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
   const kickstarterURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json";
+
+  const layoutStyle = {
+    gridTemplateColumns: "1000px auto",
+  }
 
   // fetch data 
   useEffect(() => {
@@ -31,7 +36,9 @@ export default function Treemap() {
         games: jsons[0].children.map(getCategories),
         movies: jsons[1].children.map(getCategories),
         kickstart: jsons[2].children.map(getCategories),
-      })
+      });
+
+      setCurCategories(jsons[0].children.map(getCategories));
     }
 
     function getCategories(dataObj) {
@@ -42,14 +49,7 @@ export default function Treemap() {
 
   // setting up treemap
   useEffect(() => {
-    let curCategories = [];
     const tooltip = window.d3.select("#tooltip");
-    const legend = window.d3.select("#legend");
-
-    if (Object.keys(categories).length) {
-      curCategories = Object.values(categories.games);
-    }
-
     const colors = [
       "#fcff63",
       "#ff875f",
@@ -76,6 +76,7 @@ export default function Treemap() {
       .range(colors);
 
     function drawTreeMap() {
+      console.log(curCategories);
       const root = window.d3.hierarchy(gameSaleData)
         .sum(child => child.value)
         .sort((a, b) => b.value - a.value);
@@ -87,13 +88,17 @@ export default function Treemap() {
 
       treemap(root);
 
-      svg.attr("width", w + 200)
+      svg.attr("width", w)
         .attr("height", h)
         .style("background-color", "#fff");
 
-      svg.selectAll("rect")
+      const group = svg.selectAll("g")
         .data(root.leaves())
-        .enter().append("rect")
+        .enter()
+        .append("g")
+        .attr("class", "grouping");
+        
+      group.append("rect")
         .attr("class", "tile")
         .attr("x", d => d.x0)
         .attr("y", d => d.y0)
@@ -106,15 +111,16 @@ export default function Treemap() {
         .on("mouseover", onMouseOver)
         .on("mouseout", onMouseOut);
 
-      svg.selectAll("text")
-        .data(root.leaves())
-        .enter()
-        .append("text")
-        .text(d => d.data.name)
-        .attr("x", d => d.x0)
-        .attr("y", d => d.y0)
+      group.append("text")
+        .attr("transform", d => `translate(${d.x0 + 5}, ${d.y0 + 10})`)
         .style("font-size", 10)
-        .attr("transform", `translate(5, 10)`);
+        .selectAll("tspan")
+        .data(d => d.data.name.split(/(?=[A-Z][^A-Z])/g))
+        .enter()
+        .append("tspan")
+        .text(gameTitle => gameTitle)
+        .attr("x", 0)
+        .attr("y", (d, i) => i * 10)
 
       function onMouseOver(e) {
         const x = e.pageX;
@@ -122,11 +128,7 @@ export default function Treemap() {
         const name = e.target.getAttribute("data-name");
         const category = e.target.getAttribute("data-category")
         const info = gameSaleData.children
-          .filter(cat => {
-            if (cat.name === category) {
-              return cat.children;
-            }
-          })[0].children
+          .filter(cat => cat.name === category)[0].children
           .filter(catItem => catItem.name === name)[0];
         tooltip
           .style("top", y + "px")
@@ -147,13 +149,19 @@ export default function Treemap() {
       }
 
       // setting up legend
+      const legendSVG = window.d3.select("#legend-svg")
+        .attr("width", 200)
+        .attr("height", h);
       const legendItemWidth = 20;
-      const legendItem = legend.selectAll("g")
+      const legendItem = legendSVG.select("#legend")
+        .attr("x", 0)
+        .attr("y", 0)
+        .selectAll("g")
         .data(curCategories)
         .enter()
         .append("g")
         .attr("class", "legend-item-grouping")
-        .attr("transform", `translate(${w + 50}, 20)`)
+        .attr("transform", `translate(10, 20)`);
 
       legendItem.append("rect")
         .attr("width", legendItemWidth)
@@ -169,19 +177,29 @@ export default function Treemap() {
 
     }
 
-    if (gameSaleData) drawTreeMap();
+    if (gameSaleData) {
+      console.log("drawing" + Object.keys(gameSaleData).length)
+      drawTreeMap()
+    };
 
-  }, [gameSaleData, categories])
+  }, [gameSaleData, categories, svg, curCategories])
   return (
     <main>
       <h2 id="title">Treemap - Video Game Sales</h2>
       <section>
         <p id="description">Top 100 Most Sold Video Games Grouped by Platform</p>
-        <div className="graph-container">
+        <div className="graph-container" style={layoutStyle}>
           <div id="tooltip" style={{ padding: "0em 1em" }}></div>
           <svg className="graph" ref={svgRef}>
+          </svg>
+          <svg id="legend-svg" style={{justifySelf: "start"}}>
             <g id="legend"></g>
           </svg>
+        </div>
+        <div className="button-container">
+          <button className="games-info"> Games </button>
+        <button className="movies-info" onClick={() => console.log("change the dataset") }> Movies </button>
+        <button className="kickstart-info"> Kick Starter Pledges </button>
         </div>
       </section>
     </main>
